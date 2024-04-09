440a0f1f160863798a2c2682ec8e3b03c6348656
tiesen243 <ttien56906@gmail.com>
2024-04-09 19:45:46 +0700

    huhi

--- env.mjs
+++ env.mjs
@@ -5,14 +5,12 @@ export const env = createEnv({
   server: {
     NODE_ENV: z.enum(['development', 'test', 'production']),
     DATABASE_URL: z.string(),
-    NEXTAUTH_SECRET: z.string(),
   },
   client: {},
 
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
     DATABASE_URL: process.env.DATABASE_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   },
 
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
