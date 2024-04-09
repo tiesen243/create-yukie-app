73ffa66a0d9d2dc9bcefe10f36444965da70e01f
tiesen243 <ttien56906@gmail.com>
2024-04-09 21:00:38 +0700

    dsad

--- lib/api/server.ts
+++ lib/api/server.ts
@@ -5,3 +5,10 @@ import { headers } from 'next/headers'
 import { getBaseUrl } from '@/lib/site'
 
 export const api = treaty<App>(getBaseUrl(), { headers: headers() }).api.elysia
+
+/*
+ * I dont know why it's not working when building the app
+ * and throwing an error like this:
+ * Invariant: headers() expects to have requestAsyncStorage, none available.
+ * But it's working fine when running the app in development mode.
+ */
