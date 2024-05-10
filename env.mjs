756a83605d5be97ee3b0ae62ca74af03c75d0d33
Tiesen <ttien56906@gmail.com>
2024-05-10 17:33:45 +0700

    chore: change to vercel postpres

--- env.mjs
+++ env.mjs
@@ -5,7 +5,14 @@ export const env = createEnv({
   // Server environment variables
   server: {
     NODE_ENV: z.enum(['development', 'test', 'production']),
-    DATABASE_URL: z.string(),
+    POSTGRES_URL: z.string(),
+    POSTGRES_PRISMA_URL: z.string(),
+    POSTGRES_URL_NO_SSL: z.string(),
+    POSTGRES_URL_NON_POOLING: z.string(),
+    POSTGRES_USER: z.string(),
+    POSTGRES_HOST: z.string(),
+    POSTGRES_PASSWORD: z.string(),
+    POSTGRES_DATABASE: z.string(),
   },
 
   // Client environment variables
@@ -13,7 +20,14 @@ export const env = createEnv({
 
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
-    DATABASE_URL: process.env.DATABASE_URL,
+    POSTGRES_URL: process.env.POSTGRES_URL,
+    POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
+    POSTGRES_URL_NO_SSL: process.env.POSTGRES_URL_NO_SSL,
+    POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
+    POSTGRES_USER: process.env.POSTGRES_USER,
+    POSTGRES_HOST: process.env.POSTGRES_HOST,
+    POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+    POSTGRES_DATABASE: process.env.POSTGRES_DATABASE,
   },
 
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
