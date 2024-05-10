0f2e9df248cb4a1ce3e996c83cf17648bcd968f9
Tiesen <ttien56906@gmail.com>
2024-05-10 22:59:05 +0700

    chore: add loading to theme button

--- components/theme-btn.tsx
+++ components/theme-btn.tsx
@@ -13,7 +13,7 @@ export const ThemeBtn: React.FC = () => {
   // NOTE: Disable rendering on the server
   const [isMounted, setIsMounted] = React.useState(false)
   React.useEffect(() => setIsMounted(true), [])
-  if (!isMounted) return <Button variant="ghost" size="icon" />
+  if (!isMounted) return <Button variant="ghost" size="icon" isLoading />
 
   return (
     <Button variant="ghost" size="icon" onClick={toggleTheme}>
