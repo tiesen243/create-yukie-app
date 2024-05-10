b7cdda226cbde7631b52cbd84238fd38c7f85588
Tiesen <ttien56906@gmail.com>
2024-05-10 18:24:29 +0700

    feat: add cron job to delete expired sessions every week

--- server/elysia.ts
+++ server/elysia.ts
@@ -1,9 +1,25 @@
+import { cron } from '@elysiajs/cron'
 import { Elysia } from 'elysia'
 
+import { lucia } from '@/server/auth/lucia'
 import { postRoute } from '@/server/routes/post.route'
 import { userRoute } from '@/server/routes/user.route'
 
-const app = new Elysia({ prefix: '/api' }).use(userRoute).use(postRoute).compile()
+const app = new Elysia({ prefix: '/api' })
+  .use(
+    // cron job to delete expired sessions every week
+    cron({
+      name: 'delete expired sessions',
+      pattern: '0 0 * * 0',
+      run: async () => {
+        await lucia.deleteExpiredSessions()
+      },
+    }),
+  )
+
+  .use(userRoute)
+  .use(postRoute)
+  .compile()
 
 export const handler = app.handle
 
